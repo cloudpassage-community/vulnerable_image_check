9496a2ef077d0c4d4e3ef50d63d7e60b457acba7
jgibbons-cp <jgibbons@cloudpassage.com>
2018-02-22 16:21:36 -0600

    First commit

---
+++ test/unit/test_config_helper.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import imp
+import unittest
+
+# import modules
+here_dir = os.path.dirname(os.path.abspath(__file__))
+
+module_name = 'config_helper'
+module_path = os.path.join(here_dir, '../../app/vulnerable_image_check/')
+sys.path.append(module_path)
+fp, pathname, description = imp.find_module(module_name)
+config = imp.load_module(module_name, fp, pathname, description)
+
+
+class TestConfigHelper(unittest.TestCase):
+    '''
+           Test the configuration
+    '''
+    def setUp(self):
+        pass
+
+    def test_config(self):
+        '''
+            Test if the variable is set
+        '''
+
+        # error if the configuration is not set
+        # with self.assertRaises(SystemExit):
+        config.ConfigHelper()
+
+
+if __name__ == '__main__':
+    unittest.main()
