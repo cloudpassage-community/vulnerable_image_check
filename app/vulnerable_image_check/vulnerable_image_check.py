854129b33ff035a490fc25f8c024a84e761cae4e
jgibbons-cp <jgibbons@cloudpassage.com>
2018-02-22 14:28:27 -0600

    Chg: clean up, document, add support for all stdout to use reporting if there are vulnerabilities

--- app/vulnerable_image_check/vulnerable_image_check.py
+++ app/vulnerable_image_check/vulnerable_image_check.py
@@ -1,39 +1,85 @@
+import os
+import cloudpassage
+
+
 class VulnerableImageCheck(object):
     """
        This class will return information for vulnerable images in monitored
        registries
     """
-    def __init__(self, halo):
-        self.vulnerable_image_check(halo)
+    def __init__(self):
+        # no initialization
+        pass
 
     @classmethod
-    def vulnerable_image_check(self, halo):
+    def vulnerable_image_check(self, config):
+        """We use this class to scan images for vulnerabilities."""
 
-        # get an HTTP helper object to call REST endpionts
-        http_helper_ob = halo.get_http_helper_obj()
-
-        # get all the critical active issues
-        image_issues_endpoint = "/v1/image_issues?critical=true&status=active"
-        image_issues = http_helper_ob.get(image_issues_endpoint)
-
-        count = 0
         NONE = 0
-        INCREMENT = 1
-        NEWLINE = "\n"
+        FAIL = "1"
 
-        for image_issue in image_issues["image_issues"]:
-            print "%sImage in repository %s with tag %s from registry %s" \
-                  " has vulnerable package %s." \
-                  % (NEWLINE, image_issue["image"]["repository"]["name"],
-                     image_issue["image"]["tag"],
-                     image_issue["image"]["registry"]["name"],
-                     image_issue["name"])
+        # authenticate to get a session object
+        self.session = cloudpassage.HaloSession(config.halo_key,
+                                                config.halo_secret)
 
-            count += INCREMENT
+        # get an HTTP helper object to call REST endpoints
+        http_helper = cloudpassage.HttpHelper(self.session)
 
-        # if there are any critical vulnerabilities then kill job
-        if count == NONE:
-            print "There are no vulnerable images in the monitored registries"
+        # get all the critical, active issues
+        image_issues_endpoint = \
+            "/v1/image_issues?critical=true&status=active"
+
+        # set any query parameters set in environment
+        image_issues_endpoint = \
+            self.paramaterize_images_issues_endpoint(image_issues_endpoint,
+                                                     config)
+
+        # check for vulnerabilities
+        image_issues = http_helper.get(image_issues_endpoint)
+
+        if image_issues["count"] != NONE:
+            os.environ["FAIL_ON_CRITICAL"] = FAIL
         else:
-            raise ValueError("Critical vulnerabilities in images... "
-                             "failing job")
+            image_issues = {}
+
+        # issue vulnerability data if exists
+        return image_issues
+
+    @classmethod
+    def paramaterize_images_issues_endpoint(cls, image_issues_endpoint,
+                                            config):
+        """
+        Set query parameters if in the environment.  If an environment variable
+        is not set or is empty it will return everything for that variable
+
+        Args:
+            - cls - reference to the current instance of hte class
+            - image_issues_endpoint (str) - the original REST endpoint
+            - config (instance) - instance with the config variables
+
+        Returns:
+            - image_issues_endpoint (str) - the final endpoint
+        """
+
+        EMPTY = ""
+
+        # if the environment has set the config registry_name set it in the
+        # endpoint
+        if config.registry_name is not None and config.registry_name != EMPTY:
+            image_issues_endpoint += "&registry_name=%s" % config.registry_name
+
+        # if the environment has set the config repository_name set it in the
+        # endpoint
+        if config.repository_name is not None and \
+                config.registry_name != EMPTY:
+            image_issues_endpoint += \
+                "&repository_name=%s" % config.repository_name
+
+        # if the environment has set the config image_tag set it in the
+        # endpoint
+        if config.image_tag is not None and config.registry_name != EMPTY:
+            image_issues_endpoint += \
+                "&image_tag=%s" % config.image_tag
+
+        # return the endpoint
+        return image_issues_endpoint
