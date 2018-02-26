ef7b9d74802446bce6ef085fb3bf4c4a43cb7beb
jgibbons-cp <jgibbons@cloudpassage.com>
2018-02-26 12:59:33 -0800

    @wip: move a few items around for testing and fix a few checks that had errors

--- app/vulnerable_image_check/vulnerable_image_check.py
+++ app/vulnerable_image_check/vulnerable_image_check.py
@@ -7,20 +7,18 @@ class VulnerableImageCheck(object):
        This class will return information for vulnerable images in monitored
        registries
     """
-    def __init__(self):
-        # no initialization
-        pass
+    def __init__(self, config):
+        self.config = config
 
-    @classmethod
-    def vulnerable_image_check(self, config):
+        # authenticate to get a session object
+        self.session = cloudpassage.HaloSession(self.config.halo_key,
+                                                self.config.halo_secret)
+
+    def vulnerable_image_check(self):
         """We use this class to scan images for vulnerabilities."""
 
         NONE = 0
         FAIL = "1"
-
-        # authenticate to get a session object
-        self.session = cloudpassage.HaloSession(config.halo_key,
-                                                config.halo_secret)
 
         # get an HTTP helper object to call REST endpoints
         http_helper = cloudpassage.HttpHelper(self.session)
@@ -31,8 +29,7 @@ class VulnerableImageCheck(object):
 
         # set any query parameters set in environment
         image_issues_endpoint = \
-            self.paramaterize_images_issues_endpoint(image_issues_endpoint,
-                                                     config)
+            self.paramaterize_images_issues_endpoint(image_issues_endpoint)
 
         # check for vulnerabilities
         image_issues = http_helper.get(image_issues_endpoint)
@@ -45,9 +42,7 @@ class VulnerableImageCheck(object):
         # issue vulnerability data if exists
         return image_issues
 
-    @classmethod
-    def paramaterize_images_issues_endpoint(cls, image_issues_endpoint,
-                                            config):
+    def paramaterize_images_issues_endpoint(self, image_issues_endpoint):
         """
         Set query parameters if in the environment.  If an environment variable
         is not set or is empty it will return everything for that variable
@@ -55,7 +50,6 @@ class VulnerableImageCheck(object):
         Args:
             - cls - reference to the current instance of hte class
             - image_issues_endpoint (str) - the original REST endpoint
-            - config (instance) - instance with the config variables
 
         Returns:
             - image_issues_endpoint (str) - the final endpoint
@@ -65,21 +59,24 @@ class VulnerableImageCheck(object):
 
         # if the environment has set the config registry_name set it in the
         # endpoint
-        if config.registry_name is not None and config.registry_name != EMPTY:
-            image_issues_endpoint += "&registry_name=%s" % config.registry_name
+        if self.config.registry_name is not None and \
+                self.config.registry_name != EMPTY:
+            image_issues_endpoint += \
+                "&registry_name=%s" % self.config.registry_name
 
         # if the environment has set the config repository_name set it in the
         # endpoint
-        if config.repository_name is not None and \
-                config.registry_name != EMPTY:
+        if self.config.repository_name is not None and \
+                self.config.repository_name != EMPTY:
             image_issues_endpoint += \
-                "&repository_name=%s" % config.repository_name
+                "&repository_name=%s" % self.config.repository_name
 
         # if the environment has set the config image_tag set it in the
         # endpoint
-        if config.image_tag is not None and config.registry_name != EMPTY:
+        if self.config.image_tag is not None and \
+                self.config.image_tag != EMPTY:
             image_issues_endpoint += \
-                "&image_tag=%s" % config.image_tag
+                "&image_tag=%s" % self.config.image_tag
 
         # return the endpoint
         return image_issues_endpoint
